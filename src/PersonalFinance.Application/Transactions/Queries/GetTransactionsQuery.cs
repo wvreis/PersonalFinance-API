9de4e7c746cdc5c92901c5166b65949b162a865e
Wesley Valentim <wv.reis@gmail.com>
2023-10-11 21:05:25 -0300

    FIx transaction searchInfo error.

--- src/PersonalFinance.Application/Transactions/Queries/GetTransactionsQuery.cs
+++ src/PersonalFinance.Application/Transactions/Queries/GetTransactionsQuery.cs
@@ -4,5 +4,5 @@ namespace PersonalFinance.Application.Transactions.Queries;
 
 public class GetTransactionsQuery : IRequest<List<TransactionDto>>
 {
-    public string SearchInfo { get; set; } = string.Empty;
+    public string? SearchInfo { get; set; } = string.Empty;
 }
