16af1112ef94cffbeb8520d6e05444d388d2a111
Wesley Valentim <wv.reis@gmail.com>
2023-11-18 15:02:18 -0300

    Testing restap component nuget package.

---
+++ src/PersonalFinanceUI/PersonalFinanceUI.Client/Models/BankDto.cs
@@ -0,0 +1,8 @@
+namespace PersonalFinanceUI.Client.Models;
+
+public class BankDto
+{
+    public int Id { get; set; }
+    public int Number { get;  set; }
+    public string Name { get; set; } = string.Empty;
+}
