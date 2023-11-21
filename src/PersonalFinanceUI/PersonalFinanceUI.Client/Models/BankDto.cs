5bc44e733b016d040dbe0dbafa8250fcef49ef41
Wesley Valentim <wv.reis@gmail.com>
2023-11-21 17:22:59 -0300

    Usong Dtos from Shared project.

--- src/PersonalFinanceUI/PersonalFinanceUI.Client/Models/BankDto.cs
+++
@@ -1,8 +0,0 @@
-namespace PersonalFinanceUI.Client.Models;
-
-public class BankDto
-{
-    public int Id { get; set; }
-    public int Number { get;  set; }
-    public string Name { get; set; } = string.Empty;
-}
